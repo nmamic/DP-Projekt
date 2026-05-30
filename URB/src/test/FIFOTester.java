8150c67042a21215195442735635695498343f9c
ivanarizner <ivanarizner@gmail.com>
2026-05-30 14:56:18 +0200

    implementacija FIFO

---
+++ URB/src/test/FIFOTester.java
@@ -0,0 +1,35 @@
+package test;
+
+import URBComponents.FIFOProces;
+import components.Linker;
+import components.ListenerThread;
+
+public class FIFOTester {
+    public static void main(String[] args) throws Exception {
+        String baseName = args[0];
+        int myId = Integer.parseInt(args[1]);
+        int numProc = Integer.parseInt(args[2]);
+
+        Linker linker = new Linker(baseName, myId, numProc);
+        FIFOProces fifo = new FIFOProces(linker);
+
+        // pokreni listener dretve za sve ostale procese
+        for (int i = 0; i < numProc; i++) {
+            if (i != myId) {
+                new ListenerThread(i, fifo).start();
+            }
+        }
+
+        // pošalji VIŠE poruka zaredom od istog procesa
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("[" + myId + "] Slanje poruke broj " + i);
+            fifo.FIFO_Broadcast("Poruka_" + i + "_od_procesa_" + myId);
+
+            // mali delay da poruke stignu redom kroz mrežu
+            Thread.sleep(100);
+        }
+
+        System.out.println("[" + myId + "] Sve poruke poslane, čekam na drive.");
+        Thread.sleep(2000);
+    }
+}
