8daa5d2e08e0da8541a1306e570732d7fbbaed61
Nikola M <nikolamamic5@gmail.com>
2026-05-30 15:26:20 +0200

    Popravljena bazna implementacija

--- URB/src/test/URBTester.java
+++ URB/src/test/URBTester.java
@@ -6,26 +6,30 @@ import components.ListenerThread;
 
 public class URBTester {
     public static void main(String[] args) throws Exception {
-        //svi procesi braodcastaju svoje poruke
         String baseName = args[0];
         int myId = Integer.parseInt(args[1]);
         int numProc = Integer.parseInt(args[2]);
 
         Linker linker = new Linker(baseName, myId, numProc);
-
-        //TODO: tu možeš staviti onako po argumentu ulaznom odabir koji URB algoritam koristimo
         URBProces urb = new URBProces(linker);
 
-        //pokrenem listener dretve
-        for (int i = 0; i < numProc; i++)
-            if (i != myId)
+        for (int i = 0; i < numProc; i++) {
+            if (i != myId) {
                 new ListenerThread(i, urb).start();
+            }
+        }
 
-        //proces broadcasta svoju poruku
+        Thread.sleep(500);
+
+        System.out.println("Broadcastam poruku");
         urb.URB_Broadcast("Poruka od procesa " + myId);
-        if (myId == 1) {
-            System.out.println("P1 crashao!");
-            System.exit(0);  // simuliraj ccrash - ovako nekako?
+
+        Thread.sleep(2000 * (numProc + 3));
+
+        System.out.println("P" + myId + " gotov.");
+        System.out.println("Dostavljene poruke: ");
+        for (String msg : urb.getDelivered()) {
+            System.out.println("  " + msg);
         }
     }
 }
